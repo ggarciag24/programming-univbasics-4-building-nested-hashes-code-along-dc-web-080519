10e6ba87f1ce01181873eec4dcd9122ddabc6564
German Garcia <ggarciag.business@outlook.com>
2019-07-25 19:46:08 +0000

    Automatically backed up by Learn

--- spec/bonus_spec.rb
+++ spec/bonus_spec.rb
@@ -1,5 +1,5 @@
-require_relative 'spec_helper'
-require_relative '../lib/bonus.rb'
+#require_relative 'spec_helper'
+#require_relative '../lib/bonus.rb'
 
 #describe "bonus" do
   # remove the 'x' from the 'xit' below to activate this test!
